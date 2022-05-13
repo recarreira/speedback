5232ebad9025a20d4adf930da065a0476a1036d5
Renata Carreira <re.carreira.11@gmail.com>
2022-05-13 20:32:10 -0300

    refactors move-around

--- src/speedback/core.clj
+++ src/speedback/core.clj
@@ -8,20 +8,19 @@
                             (/ 2)
                             int
                             inc)
-        [group1 group2] #nu/tapd (split-at number-of-pairs members)
-        group2* (-> group2
-                    reverse
-                    vec
-                    (conj pivot))]
+        [group1 group2] (split-at number-of-pairs members)
+        group2*         (-> group2
+                            reverse
+                            vec
+                            (conj pivot))]
     (partition 2 (interleave group1 group2*))))
 
 (defn move-around
   [members]
-  (concat [(peek members)] (pop members)))
+  (let [current-last-member  [(peek members)]
+        all-members-but-last (pop members)]
 
-
-
-
+   (concat current-last-member all-members-but-last)))
 
 
 (defn -main
