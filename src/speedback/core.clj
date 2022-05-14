16e2a3cdd699d63381cf703e33b2925a00c00b56
Renata Carreira <re.carreira.11@gmail.com>
2022-05-13 22:19:08 -0300

    adds generate-session-rounds

--- src/speedback/core.clj
+++ src/speedback/core.clj
@@ -1,27 +1,38 @@
 (ns speedback.core
   (:gen-class))
 
-(defn generate-round
-  [members pivot]
-  (let [number-of-pairs (-> members
-                            count
-                            (/ 2)
-                            int
-                            inc)
-        [group1 group2] (split-at number-of-pairs members)
-        group2*         (-> group2
-                            reverse
-                            vec
-                            (conj pivot))]
+(defn generate-round-pairs
+  [members]
+  (let [number-of-pairs  (-> members
+                             count
+                             (/ 2)
+                             int)
+        pivot            (peek members)
+        members-but-last (pop members)
+        [group1 group2]  (split-at number-of-pairs members-but-last)
+        group2*          (-> group2
+                             reverse
+                             vec
+                             (conj pivot))]
     (partition 2 (interleave group1 group2*))))
 
 (defn move-around
   [members]
-  (let [current-last-member  [(peek members)]
-        all-members-but-last (pop members)]
+  (let [[last-but-one pivot] (take-last 2 members)
+        members-but-last-two (drop-last 2 members)]
 
-   (concat current-last-member all-members-but-last)))
+    (vec (concat [last-but-one] members-but-last-two [pivot]))))
 
+(defn generate-session-rounds
+  [members]
+  (let [number-of-rounds (dec (count members))
+        process-round    (fn [data _round-number]
+                           {:rounds  (conj (:rounds data) (generate-round-pairs (:members data)))
+                            :members (move-around (:members data))})]
+    (-> (reduce process-round
+                {:members members :rounds []}
+                (range number-of-rounds))
+        :rounds)))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/speedback/core_test.clj
+++ test/speedback/core_test.clj
@@ -2,14 +2,23 @@
   (:require [clojure.test :refer :all]
             [speedback.core :as core]))
 
-(deftest generate-round-test
-  (testing "return pairs given participants"
+(deftest generate-round-pairs-test
+  (testing "returns pairs given participants"
     (is (= [[1 5] [2 4] [3 6]]
-           (core/generate-round [1 2 3 4 5] 6)))
+           (core/generate-round-pairs [1 2 3 4 5 6])))
     (is (= [[1 3] [2 4]]
-           (core/generate-round [1 2 3] 4)))))
+           (core/generate-round-pairs [1 2 3 4])))))
 
 (deftest move-around-test
-  (testing "move last member to the first position"
-    (is (= [5 1 2 3 4]
-           (core/move-around [1 2 3 4 5])))))
+  (testing "moves last member to the first position"
+    (is (= [5 1 2 3 4 6]
+           (core/move-around [1 2 3 4 5 6])))))
+
+(deftest generate-session-rounds-test
+  (testing "returns all rounds for the session"
+    (is (= [[[1 5] [2 4] [3 6]]
+            [[5 4] [1 3] [2 6]]
+            [[4 3] [5 2] [1 6]]
+            [[3 2] [4 1] [5 6]]
+            [[2 1] [3 5] [4 6]]]
+           (core/generate-session-rounds [1 2 3 4 5 6])))))
