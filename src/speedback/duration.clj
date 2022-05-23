aac952dfc4d606add8e126716b441b6a20cc757c
Renata Carreira <re.carreira.11@gmail.com>
2022-05-23 18:06:43 -0300

    calculates pair duration given total time and number of participants

--- src/speedback/duration.clj
+++ src/speedback/duration.clj
@@ -1,12 +1,42 @@
-(ns speedback.duration)
+(ns speedback.duration
+  (:require [schema.core :as s]
+            [speedback.aux :as aux]))
 
-(defn suggested-duration
-  [number-of-members]
-  (let [number-of-rounds  (dec number-of-members)
-        swap-time         (dec number-of-rounds)
-        time-per-pair     10
-        introduction-time 5]
+(def Durations
+  {:total        s/Int
+   :introduction s/Int
+   :pair         s/Int
+   :swap         s/Int})
+
+(def default-introduction-time 5)
+
+(def default-swap-time 1)
+
+(s/defn ^:private calculates-total-swap-time :- s/Int
+  [number-of-rounds :- s/Int]
+  (* default-swap-time (dec number-of-rounds)))
+
+(s/defn suggested-duration :- s/Int
+  [number-of-members :- s/Int]
+  (let [number-of-rounds (aux/calculates-number-of-rounds number-of-members)
+        total-swap-time  (calculates-total-swap-time number-of-rounds)
+        time-per-pair    10]
     (-> number-of-rounds
         (* time-per-pair)
-        (+ introduction-time)
-        (+ swap-time))))
+        (+ default-introduction-time)
+        (+ total-swap-time))))
+
+(s/defn durations-by-total-time :- Durations
+  [total-time :- s/Int
+   number-of-participants :- s/Int]
+  (let [number-of-rounds (aux/calculates-number-of-rounds number-of-participants)
+        total-swap-time (calculates-total-swap-time number-of-rounds)
+        pair-time (-> total-time
+                      (- default-introduction-time)
+                      (- total-swap-time)
+                      (/ number-of-rounds)
+                      (int))]
+    {:total total-time
+     :introduction default-introduction-time
+     :swap default-swap-time
+     :pair pair-time}))
--- test/speedback/duration_test.clj
+++ test/speedback/duration_test.clj
@@ -4,6 +4,20 @@
    [speedback.duration :as duration]))
 
 (deftest suggested-duration-test
-  (testing "returns the suggested total duration given number of participants"
+  (testing "returns the suggested total duration given even number of participants"
     (is (= 59
-           (duration/suggested-duration 6)))))
+           (duration/suggested-duration 6))))
+  (testing "returns the suggested total duration given odd number of participants"
+    (is (= 59
+           (duration/suggested-duration 5)))))
+
+(deftest durations-by-total-time-test
+  (testing "returns the duration per pair given total time and number participants"
+   (is (= {:total 59
+           :introduction 5
+           :swap 1
+           :pair 10}
+          (duration/durations-by-total-time 59 6))))
+  (testing "returns the rounded duration per pair"
+   (is (= 10
+          (:pair (duration/durations-by-total-time 60 6))))))
